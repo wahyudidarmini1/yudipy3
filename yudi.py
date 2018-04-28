363828756bc6730199438fddf8ca1b940786aa36
wahyudidarmini1 <37975743+wahyudidarmini1@users.noreply.github.com>
2018-04-28 19:38:40 +0700

    Update yudi.py

--- yudi.py
+++ yudi.py
@@ -4,7 +4,7 @@ from linepy import *
 from akad.ttypes import *
 from multiprocessing import Pool, Process
 from time import sleep
-import pytz, datetime, pafy, time, timeit, random, sys, ast, re, os, json, subprocess, threading, string, codecs, requests, tweepy, ctypes, urllib, wikipedia
+import datetime, pafy, time, timeit, random, sys, ast, re, os, json, subprocess, threading, string, codecs, requests, tweepy, ctypes, urllib, wikipedia
 from datetime import timedelta, date
 from datetime import datetime
 from bs4 import BeautifulSoup
